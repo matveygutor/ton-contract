4fa941e076a31569061d0b1e8468265ed22113c8
matveygutor <matweyhutorts@gmail.com>
2024-08-06 22:23:05 +0300

    Workflow test

--- src/hooks/useMainContract.ts
+++ src/hooks/useMainContract.ts
@@ -34,7 +34,7 @@ export function useMainContract() {
       if (!mainContract) return;
       setContractData(null);
       const val = await mainContract.getData();
-      const { balance } = (await mainContract.getBalance()).number;
+      const balance:number  = (await mainContract.getBalance()).number;
       setContractData({
         counter_value: val.number,
         recent_sender: val.recent_sender,
